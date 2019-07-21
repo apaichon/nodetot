772d2c2bdd10f634fdc20527aa86cb245c5cafaa
unknown <apaichon@gmail.com>
2019-07-22 01:48:16 +0700

    lab3 promise

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const {lab1, lab2} = require('./labs')
 
 lab1()
 
-lab2.ReadCSV('./data/premiere_2018-2019.csv', (result) => {})
-lab2.ReadCSV('./data/liverpoolPlayers.json',(result)=> {})
+// lab2.ReadCSV('./data/premiere_2018-2019.csv', (result) => {})
+// lab2.ReadCSV('./data/liverpoolPlayers.json',(result)=> {})
+
+lab2.readFile('./data/premiere_2018-2019.csv')
+  .then((premiere) => lab2.readFile('././data/liverpoolPlayers.json'))
+  .then((liverpool) => console.log(liverpool))
+  .catch(err => console.log(err))
--- libs/File.js
+++ libs/File.js
@@ -8,6 +8,21 @@ class File {
       callback(data)
     });
   }
+  static readFile(fileName) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(fileName, 'utf8', (err, data) => {
+        if (err) reject(err);
+        console.log('-----Reading----', fileName)
+        resolve(data)
+      });
+    })
+  }
+
+  static async readFileAsync (fileName) {
+    return await File.readFile(fileName);
+  }
+
+  
 }
 
 module.exports = File
